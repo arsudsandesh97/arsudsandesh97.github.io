dedfeac05aff4d84c703a455bbf5e34dde36858d
Sandesh Arsud <arsudsandesh97@gmail.com>
2025-11-23 15:19:38 +0530

    CRITICAL: Replace Stars.js with dummy component to eliminate WebGL completely

--- src/components/canvas/Stars.js
+++ src/components/canvas/Stars.js
@@ -1,69 +1,10 @@
-import React, { useRef, useState, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
-import styled from "styled-components";
+// This component has been disabled to prevent WebGL crashes
+// on devices that don't support WebGL or have it disabled
 
-const StyledCanvasWrapper = styled.div`
-  width: 100%;
-  height: auto;
-  position: absolute;
-  inset: 0;
-`;
-
-const Stars = (props) => {
-  const ref = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
-  );
-
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
-
-  return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
-          transparent
-          color="#f272c8"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
-    </group>
-  );
-};
+import React from 'react';
 
 const StyledStarsCanvas = () => {
-  const [isWebGLSupported, setIsWebGLSupported] = useState(false);
-
-  React.useEffect(() => {
-    try {
-      const canvas = document.createElement("canvas");
-      const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-      if (gl) {
-        setIsWebGLSupported(true);
-      }
-    } catch (e) {
-      console.warn("WebGL check failed:", e);
-    }
-  }, []);
-
-  if (!isWebGLSupported) return null;
-
-  return (
-    <StyledCanvasWrapper>
-      <Canvas camera={{ position: [0, 0, 1] }} gl={{ preserveDrawingBuffer: true }}>
-        <Suspense fallback={null}>
-          <Stars />
-        </Suspense>
-        <Preload all />
-      </Canvas>
-    </StyledCanvasWrapper>
-  );
+  return null;
 };
 
 export default StyledStarsCanvas;
