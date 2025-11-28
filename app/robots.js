aee074892e066b11b5aeaaf84eff5f32673d23a5
Sandesh Arsud <arsudsandesh97@gmail.com>
2025-11-28 14:08:39 +0530

    Remove sitemap-v2

--- app/robots.js
+++ app/robots.js
@@ -35,7 +35,6 @@ export default function robots() {
     sitemap: [
       'https://arsudsandesh97.github.io/sitemap.xml',
       'https://arsudsandesh97.github.io/sitemap_index.xml',
-      'https://arsudsandesh97.github.io/sitemap-v2/sitemap.xml',
     ],
   };
 }
